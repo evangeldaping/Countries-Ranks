8a464ed70469338b4d463863ca2f1a287eb8429b
evangel <evangeldaping@gmail.com>
2021-11-12 01:37:50 +0800

    Filter Countries by Name and Region

--- countries-ranks/src/pages/index.js
+++ countries-ranks/src/pages/index.js
@@ -2,16 +2,34 @@ import Layout from '../components/Layout/Layout'
 import styles from '../styles/Home.module.css'
 import SearchInput from '../components/SearchInput/SearchInput'
 import CountriesTable from '../components/CountriesTable/CountriesTable';
+import { useState } from "react";
 
 export default function Home({ countries }) {
   console.log(countries);
+
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.official.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword)
+    );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  };
     return (
       <Layout>
         <div className={styles.counts}>Found {countries.length} countries</div>
       
-        <SearchInput placeholder="Filter by Name, Region or SubRegion"/>
+        <SearchInput 
+          placeholder="Filter by Name or Region" 
+          onChange={onInputChange}
+        />
       
-        <CountriesTable countries={countries}/>
+        <CountriesTable countries={filteredCountries}/>
       </Layout>
     );
 }
