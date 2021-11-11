43324db7bdf58a1419286bb281388b03faeba741
evangel <evangeldaping@gmail.com>
2021-11-11 22:36:54 +0800

    getStaticProps

--- countries-ranks/src/pages/index.js
+++ countries-ranks/src/pages/index.js
@@ -1,6 +1,22 @@
 import Layout from '../components/Layout/Layout'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-    return <Layout>main</Layout>;
+export default function Home({ countries }) {
+  console.log(countries);
+    return (
+      <Layout>
+        <div className={styles.counts}>Found {countries.length}</div>
+      </Layout>
+    );
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.com/v3.1/all');
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
 }
